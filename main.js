724ad933bc083b71618c917e406db2a2ccc13adc
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-03-10 08:33:33 +0100

    splash screen and home page

--- main.js
+++ main.js
@@ -1,24 +1,54 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+require('electron-reload')(__dirname);
 
 let win;
+let splash;
 
-function createWindow() {
-  win = new BrowserWindow({
+function createSplashWindow() {
+  splash = new BrowserWindow({
     width: 900,
     height: 700,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    resizable: false,
     webPreferences: {
       nodeIntegration: true
     }
   });
 
-  win.loadFile('./pages/home.html');
+  splash.loadFile(path.join(__dirname, 'pages/splash.html'));
+
+  splash.once('ready-to-show', () => {
+    splash.show();
+    setTimeout(createMainWindow, 2000);
+  });
+}
+
+function createMainWindow() {
+  win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'pages/home.html'));
+
+  win.once('ready-to-show', () => {
+    win.show();
+    splash.close();
+  });
 
   win.on('closed', () => {
     win = null;
   });
 }
 
-app.on('ready', createWindow);
+app.on('ready', createSplashWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -28,6 +58,6 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createSplashWindow();
   }
 });
